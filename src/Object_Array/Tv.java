83bbe6dd5d5cbf6c932caeffb0755584fa645ca1
changseon <chaosen0521@gmail.com>
2023-10-18 16:50:46 +0900

    클래스 TV

---
+++ src/Object_Array/Tv.java
@@ -0,0 +1,38 @@
+package Object_Array;
+
+import java.util.Scanner;
+
+public class Tv {
+    int select;
+    static int volume;
+    static int channel;
+
+    static boolean power;
+
+    void setVolume(int a){
+        a=volume;
+        System.out.println("현재 볼륨:"+volume);
+
+
+    }
+    void setChannel(){
+
+    }
+    void setPower(){
+
+    }
+    void ChannelUp(){
+
+    }
+    void ChannelDown(){
+
+    }
+
+    void VolumeUp(){
+
+    }
+    void VolumeDown(){
+
+    }
+
+}
