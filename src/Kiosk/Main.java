9534fb458d2405fe836190f79a1c019ea9157c62
changseon <chaosen0521@gmail.com>
2023-10-19 20:13:52 +0900

    Kiosk_Main

---
+++ src/Kiosk/Main.java
@@ -0,0 +1,13 @@
+package Kiosk;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        menu.addMenu("test","test");
+        menu.addProduct("test","test","test",1.2);
+
+    }
+}
+
